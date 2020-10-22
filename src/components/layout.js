24c4fac324003aaeb2da94139c51a84e687d7154
Maarten Louage <me@maartenlouage.nl>
2020-10-22 13:04:21 +0200

    Add slidehow

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+import 'react-slideshow-image/dist/styles.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
---
+++ src/components/slideshow.js
@@ -0,0 +1,25 @@
+
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+
+const slideImages = [
+    '/static/77e184939cd1f8f52289b2844bf4f8ba/2244e/xxl_kfz63354356_img_9845.jpg',
+    '/static/bedebffd1a407249625bb21d98b3a6b9/2244e/xxl_kfz63354356_img_9846.jpg',
+    '/static/a2858a8e8bc61a5745a3eff4af2237cb/2244e/xxl_kfz63354356_img_9847.jpg'
+];
+
+const Slideshow = () => {
+    return (
+        <div>
+            <Slide easing="ease" autoplay={false} transitionDuration={250}>                
+                {slideImages.map((each, index) => (
+                        <div key={index} className="each-slide">
+                            <div style={{ backgroundImage: `url(${each})` }} />
+                        </div>
+                ))}
+            </Slide>
+        </div>
+    )
+};
+
+export default Slideshow;
