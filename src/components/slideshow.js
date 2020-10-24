5fd2fbaf599d43d9338d551c3e443ef57b2ffb88
Maarten Louage <me@maartenlouage.nl>
2020-10-24 11:22:11 +0200

    Add tailwind

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { Slide } from 'react-slideshow-image';
 
 const slideImages = [
-    '/static/77e184939cd1f8f52289b2844bf4f8ba/2244e/xxl_kfz63354356_img_9845.jpg',
-    '/static/bedebffd1a407249625bb21d98b3a6b9/2244e/xxl_kfz63354356_img_9846.jpg',
-    '/static/a2858a8e8bc61a5745a3eff4af2237cb/2244e/xxl_kfz63354356_img_9847.jpg'
+    '/static/77e184939cd1f8f52289b2844bf4f8ba/xxl_kfz63354356_img_9845.jpg',
+    '/static/bedebffd1a407249625bb21d98b3a6b9/xxl_kfz63354356_img_9846.jpg',
+    '/static/a2858a8e8bc61a5745a3eff4af2237cb/xxl_kfz63354356_img_9847.jpg'
 ];
 
 const Slideshow = () => {
