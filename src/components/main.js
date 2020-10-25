61b688767e96321d703870e75ae68062969ccdad
Maarten Louage <mlouage@users.noreply.github.com>
2020-10-25 16:48:47 +0100

    Add social header and navigation (#2)
    * Add social header and navigation
    
    * Update font sizes for social header

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,12 @@
 import React from "react"
 
 const Main = ({children}) => (
-    <>
-        <div>
+    <div className="bg-gray-200">
+        <div className="p-8">
             {children}
         </div>
         
-    </>
+    </div>
 )
 
 export default Main;
