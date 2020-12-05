ffa895385675be97863302ac097139df8a09b04c
Maarten Louage <me@maartenlouage.nl>
2020-12-05 10:04:45 +0100

    Fill in car detail page

--- src/templates/car-detail.js
+++ src/templates/car-detail.js
@@ -6,20 +6,77 @@ import Car from "../components/car"
 export const query = graphql`
     query($carID: String!) {
         vehicle(id: { eq: $carID }) {
+            CO2Emissions
             brandName
+            carpassCheck
+            colorMetallic
+            crashed
+            cylinderCapacity
+            description
+            VAT
+            VATdeductible
+            batteryCapacity
+            batteryRental
+            bodyStyleId
+            brandId
+            builtYear(formatString: "MM/YYYY")
+            dimensionHeight
+            dimensionLength
+            dimensionWidth
+            enginePowerKw
+            firstRegistration(formatString: "MM/YYYY")
+            fiscalHorsePower
+            fuelTypeId
+            gearboxId
+            horsePower
+            id
+            images
+            inStock
+            inspectionValidUntil
+            interiorMmaterialId
+            interiorMainColorId
+            interiorSecondaryColorId
+            kilometers
+            modelId
             modelName
-            version
-            price
+            nonSmokingVehicle
+            numberOfCylinders
+            numberOfDoors
+            numberOfGears
+            numberOfPreviousOwners
+            numberOfSeats
+            onDiscount
+            paintTypeId
             options
+            pollutionClassName
+            price
+            priceExclTax
+            professionalPrice
+            purchasePrice
+            serviceCheckBook
+            slug
+            smokeFilter
+            sold
+            totalPriceOfRepairs
+            transmissionId
+            trunkCapacity
+            vehicleType
+            version
+            weight
+            vin
+            warrantyMonths
+            gearBox
+            fuelType
+            transmission
         }
     }
 
 `;
 
-const CarDetailTemplate = ({ data }) => (
+const CarDetailTemplate = ({ data: { vehicle } }) => (
     <>
-        <Car />
-        <pre>{JSON.stringify(data, null, 4)}</pre>
+        <Car vehicle={ vehicle }/>
+        <pre>{JSON.stringify(vehicle, null, 4)}</pre>
     </>
 );
 
