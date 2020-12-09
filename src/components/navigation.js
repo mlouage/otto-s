e72621ae5bca1c59ff59f0a780ae65389536007b
Maarten Louage <me@maartenlouage.nl>
2020-12-09 09:43:21 +0100

    Cleanup

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Logo from "../images/Logo_2.png"
+import Logo from "../images/Logo_3.png"
 
 const Navigation = () => (
     <div className="pl-8 pr-8 h-40 md:h-32 flex flex-col md:flex-row justify-center md:justify-between items-center bg-black text-white">
