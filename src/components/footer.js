f76ad1361154b63eb8aa75372181d8b1ca81c2cd
Maarten Louage <me@maartenlouage.nl>
2021-01-28 19:08:14 +0100

    Update contact pagina

--- src/components/footer.js
+++ src/components/footer.js
@@ -9,15 +9,16 @@ const Footer = () => (
                 <div>
                     <div className="font-bold text-xl leading-none">Otto-s</div>
                     <div className="font-bold text-xl">met dat beetje meer</div>
-                    <div>Kolmenstraat 1315</div>
+                    <div>Kolmenstraat 1318</div>
                     <div>3570 Alken</div>
                     <div>Belgie</div>
-                    <div>BTW: BE 0000 000 000</div>
+                    <div>BTW: BE 076.051.1177</div>
                 </div>
                 <div>
                     <div className="font-bold text-xl">Openingsuren</div>
                     <div>Ma-Vr: 8:00- 12:00 / 13:00 - 18:00</div>
-                    <div className="mt-8"><span className="font-bold">E:</span> <a href="mailto:info@otto-s.be" className="hover:text-primary hover:underline">info@otto-s.be</a></div>
+                    <div className="mt-8"><span className="font-bold">T:</span> <a href="tel:0499149397" className="hover:text-primary hover:underline">0499 14 93 97</a></div>
+                    <div><span className="font-bold">E:</span> <a href="mailto:info@otto-s.be" className="hover:text-primary hover:underline">info@otto-s.be</a></div>
                 </div>
             </div>
         </div>
