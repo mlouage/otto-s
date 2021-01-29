011d83f9e9ea359dba321e8047e36e1fd243a82b
Maarten Louage <me@maartenlouage.nl>
2021-01-29 14:11:18 +0100

    Fix navigation on mobile

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 import Logo from "../images/Otto-s_FINAL_LOGO-03.jpg"
 
 const Navigation = () => (
-    <div className="pl-8 pr-8 h-40 md:h-48 flex flex-col md:flex-row justify-center md:justify-between items-center bg-white text-black">
+    <div className="pl-8 pr-8 h-64 md:h-48 flex flex-col md:flex-row justify-center md:justify-between items-center bg-white text-black">
         <div className="p-4 md:p-0">
             <Link to="/">
                 <img src={Logo} alt="Otto-s" />
